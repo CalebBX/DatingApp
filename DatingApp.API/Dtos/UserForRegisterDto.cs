96b802c174a34d65fc0716cc92dc9791dd026ad2
Caleb Breaux <cbrocomp@gmail.com>
2019-05-08 21:04:12 -0700

    Added user entity, Added simple JWT authentication

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(12, MinimumLength = 4, ErrorMessage ="You must specify password beween 4 and 12 characters")]
+        public string Password { get; set; }
+    }
+}
